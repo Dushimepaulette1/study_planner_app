7f415c10d2f8c18e3d8e3fdf152b0ab60f1418ce
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-10-08 18:25:55 +0200

    defined the structure of my storage to save, load and delete tasks

---
+++ lib/services/storage.dart
@@ -0,0 +1,15 @@
+import "/models/task.dart";
+
+class StorageService {
+  Future<void> saveTasks(List<Task> tasks) {
+    throw UnimplementedError('Save tasks not implemented yet');
+  }
+
+  Future<List<Task>> loadTasks() {
+    throw UnimplementedError('Load tasks not implemented yet');
+  }
+
+  Future<void> deleteTasks(List<Task> tasks) {
+    throw UnimplementedError('Delete tasks not implemented yet');
+  }
+}
