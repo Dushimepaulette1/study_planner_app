e88d85965d36a6aa592b1e6981781b980dc9f53c
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-10-08 15:00:10 +0200

    implemented tojson() and fromJson() methods

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -7,11 +7,35 @@ class Task {
   final bool isCompleted;
 
   Task({
-    required this.id,
+    String? id,
     required this.title,
     this.description,
     required this.dueDate,
     this.reminderTime,
     this.isCompleted = false,
-  });
+  }) : id = id ?? DateTime.now().millisecondsSinceEpoch.toString();
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'dueDate': dueDate.toIso8601String(),
+      'reminderTime': reminderTime?.toIso8601String(),
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      dueDate: DateTime.parse(json['dueDate']),
+      reminderTime: json['reminderTime'] != null
+          ? DateTime.parse(json['reminderTime'])
+          : null,
+      isCompleted: json['isCompleted'],
+    );
+  }
 }
