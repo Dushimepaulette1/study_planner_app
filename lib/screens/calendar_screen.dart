8256f88b33531d7f32e7e8f896d4ec97378e2685
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-10-09 14:37:48 +0200

    added an appbar and styled the app bar

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:individual_assignment_1/main.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
 
   @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("My Study Planner")));
+    return Scaffold(
+      backgroundColor: CustomColors.primaryBackground,
+      appBar: AppBar(
+        backgroundColor: CustomColors.primaryBackground,
+        title: Text(
+          "My Study Planner",
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
