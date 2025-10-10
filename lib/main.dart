438e396931ae6474a583e9c40e083f7b089fe653
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-10-10 11:33:12 +0200

    changed the colors in main.dart and also set up the light and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:study_planner_app/screens/today_screen.dart';
 import 'package:study_planner_app/screens/calendar_screen.dart';
 import 'package:study_planner_app/screens/settings_screen.dart';
 import 'package:study_planner_app/utils/colors.dart';
+import 'package:study_planner_app/services/storage_service.dart';
 
 void main() {
   runApp(const StudyPlannerApp());
@@ -16,12 +17,34 @@ class StudyPlannerApp extends StatefulWidget {
 }
 
 class _StudyPlannerAppState extends State<StudyPlannerApp> {
+  final StorageService _storageService = StorageService();
+  bool _isDarkMode = false;
   int _selectedIndex = 0;
   final List<Widget> _screens = [
     const TodayScreen(),
     const CalendarScreen(),
     const SettingsScreen(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadThemePreference();
+  }
+
+  void _loadThemePreference() async {
+    final isDarkMode = await _storageService.getThemeMode();
+    setState(() {
+      _isDarkMode = isDarkMode;
+    });
+  }
+
+  void _toggleTheme(bool isDarkMode) {
+    setState(() {
+      _isDarkMode = isDarkMode;
+    });
+    _storageService.setThemeMode(isDarkMode);
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -33,25 +56,17 @@ class _StudyPlannerAppState extends State<StudyPlannerApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Study Planner',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: AppColors.primaryBackground,
-        appBarTheme: AppBarTheme(
-          backgroundColor: AppColors.appBarColor,
-          foregroundColor: Colors.white,
-        ),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: AppColors.appBarColor,
-          selectedItemColor: AppColors.accentColor,
-          unselectedItemColor: Colors.white70,
-        ),
-        useMaterial3: true,
-      ),
+      theme: _buildLightTheme(),
+      darkTheme: _buildDarkTheme(),
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
       home: Scaffold(
         body: _screens[_selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.today), label: 'Today'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.today),
+              label: 'Today',
+            ),
             BottomNavigationBarItem(
               icon: Icon(Icons.calendar_today),
               label: 'Calendar',
@@ -65,6 +80,52 @@ class _StudyPlannerAppState extends State<StudyPlannerApp> {
           onTap: _onItemTapped,
         ),
       ),
+    );
+  }
+
+  ThemeData _buildLightTheme() {
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColors.lightPrimary,
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      cardColor: AppColors.lightSurface,
+      appBarTheme: AppBarTheme(
+        backgroundColor: AppColors.lightPrimary,
+        foregroundColor: Colors.white,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: AppColors.lightSurface,
+        selectedItemColor: AppColors.accentColor,
+        unselectedItemColor: Colors.grey,
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: AppColors.lightText),
+        bodyMedium: TextStyle(color: AppColors.lightText),
+      ),
+      useMaterial3: true,
+    );
+  }
+
+  ThemeData _buildDarkTheme() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: AppColors.darkPrimary,
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      cardColor: AppColors.darkSurface,
+      appBarTheme: AppBarTheme(
+        backgroundColor: AppColors.darkSurface,
+        foregroundColor: AppColors.darkText,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: AppColors.darkSurface,
+        selectedItemColor: AppColors.accentColor,
+        unselectedItemColor: Colors.grey,
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: AppColors.darkText),
+        bodyMedium: TextStyle(color: AppColors.darkText),
+      ),
+      useMaterial3: true,
     );
   }
 }
