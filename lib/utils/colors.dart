3041d3746c6928fe558b97af735f671549b579e8
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-10-10 11:30:56 +0200

    changed the theme of the colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryBackground = Color(0xFF011339);
-  static const Color appBarColor = Color(0xFF011339);
-  static const Color listTileColor = Color(0xFF1E2A4D);
+  // Light Theme Colors
+  static const Color lightPrimary = Color(0xFF011339);
+  static const Color lightBackground = Color(0xFFF5F5F5);
+  static const Color lightSurface = Color(0xFFFFFFFF);
+  static const Color lightText = Color(0xFF333333);
+
+  // Dark Theme Colors
+  static const Color darkPrimary = Color(0xFFBB86FC);
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkSurface = Color(0xFF1E1E1E);
+  static const Color darkText = Color(0xFFFFFFFF);
+
+  // Common Colors
   static const Color accentColor = Colors.amber;
+  static const Color successColor = Colors.green;
+  static const Color warningColor = Colors.orange;
+  static const Color errorColor = Colors.red;
 }
